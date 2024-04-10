593ac3225e512c8dd13f2bde06575ad9b51fc07c
Pratik More <pratikmore408@gmail.com>
2024-04-10 11:19:01 +0530

    bug fixed

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const searchList = document.getElementById("search-list");
 const resultGrid = document.getElementById("result-grid");
 const addToLiked = document.querySelector("#add-to-liked");
 
+console.log("script lodded");
+
 // Function to load movies from the API
 async function loadMovies(findTitle) {
-  const URL = `https://omdbapi.com/?s=${findTitle}&page=1&apikey=fc1fef96`;
+  const URL = `https://omdbapi.com/?s=${findTitle}&page=1&apikey=9fa44dd7`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
   if (data.Response == "True") displayMovieList(data.Search);
@@ -55,7 +57,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`
+        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9fa44dd7`
       );
       const movieDetails = await result.json();
       displayMovieDetails(movieDetails);

0f95915571ad08464154c0eccb589bbe3976789f
Pratik More <pratikmore408@gmail.com>
2024-04-10 11:36:23 +0530

    omdb link fixed

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9fa44dd7`
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9fa44dd7`
       );
       const movieDetails = await result.json();
       displayMovieDetails(movieDetails);
